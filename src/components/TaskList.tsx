e1c91e4fae6aaeaca3e0e2ec5c42f1d7049cf598
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-14 22:20:55 -0300

    Solução handleRemoveTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -28,11 +28,13 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    
+    setTasks(filteredTasks)
   }
 
   return (
